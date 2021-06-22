d3347216fbeecf445151631902c2b89cc63d398c
Adriel Medeiros <adriel.souza397@gmail.com>
2021-06-22 12:29:57 -0300

    Implementing login and splash screens

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { createContext, useState, useEffect } from 'react'
+import { BrowserRouter, Route } from "react-router-dom";
+import { AuthContext } from './contexts/AuthContext';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+import { auth, firebase } from './services/firebase';
+
+import { AuthContextProvider } from './contexts/AuthContext'
+
 function App() {
+  
   return (
-   <h1>Hello World</h1>
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path='/' exact component={Home} />
+        <Route path='/rooms/new' component={NewRoom} />
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
